70ad39887c402856400b6be080a7ab4a48c15692
Ida <ida_sunshine@foxmail.com>
2016-12-28 11:32:28 +0800

    Initial Commit

---
+++ FilterCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FilterCollectionView
+//
+//  Created by 腾实信 on 2016/12/28.
+//  Copyright © 2016年 ida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FilterCollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FilterCollectionView
+//
+//  Created by 腾实信 on 2016/12/28.
+//  Copyright © 2016年 ida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
